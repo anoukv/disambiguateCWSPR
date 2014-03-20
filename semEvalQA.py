a9290fa2e458da6e97a503a08fa5cb45f158b5eb
anoukv <anouk.visser@student.uva.nl>
2014-03-20 17:58:43 +0100

    implementation of Cristina's C thing in Python was completed :)

--- semEvalQA.py
+++ semEvalQA.py
@@ -8,16 +8,28 @@ from math import sqrt
 # Approved by Anouk
 def save_answers(answers, filename):
 	f = open(filename, 'w')
-	f.write( "".join([ word + "\n" for word in answers]) )
+	f.write( "".join([ str(answer[0])+" "+answer[1][0]+":"+answer[1][1]+"\n" for answer in answers]) )
 	f.close()
 
-# Written by Remi
-# Approved and edited by Anouk (made all words lower case)
 def load_questions(filename):
 	f = open(filename, 'r')
-	c = [ tuple(l.lower().replace("\n","").split(" ")) for l in f.readlines()]
+	questions = []
+	for _ in xrange(4):
+		f.readline()
+	while True:
+		l = f.readline()
+		if l not in ["", "\n", "\r\n"]:
+			questions.append(tuple(l.lower().replace("\n", "").split(":")))
+		else:
+			break
 	f.close()
-	return c
+	return questions
+
+def load_answers(filename):
+	f = open(filename, 'r')
+	answers = [ tuple(l.lower().replace("\n","").replace('"', '').split(":")) for l in f.readlines()]
+	f.close()
+	return answers 
 
 # Written by Remi
 # Approved by Anouk
@@ -53,75 +65,55 @@ def load_vectors(filename):
 	return words
 
 # Written by Anouk based on qa.c
-def qa(wordvectors, questions):
+def qa(wordvectors, questions, answers):
 	
 	# initialize empty answers list
-	answers = []
-	
-	# iterate over all questions
-	for question in questions:
+	ranking = []
 
-		# get representations for a, b and c, only if they actually exist
-		if question[0] in wordvectors and question[1] in wordvectors and question[2] in wordvectors:
-			
-			# get the word projections, this is in wordvectors[word], we assume for now that 
-			# there is only one word projection, but the content of wordvectors[word] is a list
-			# so we have to ask for index 0
-			a = wordvectors[question[0]][0]
-			b = wordvectors[question[1]][0]
-			c = wordvectors[question[2]][0]
+	for answer in answers:
+		# compute cosine similarity with all questions
+		sim = 0
+		for question in questions:
+			if question[0] in wordvectors and question[1] in wordvectors and answer[0] in wordvectors and answer[1] in wordvectors:
+				a = wordvectors[question[0]][0]
+				b = wordvectors[question[1]][0]
+				c = wordvectors[answer[0]][0]
+				d = wordvectors[answer[1]][0]
 
-			# compute v, normalize it. Result is a tuple
-			y = normalize([b[i] - a[i] + c[i] for i in xrange(len(a))])
-
-			# initialize bestSim and bestWord
-			# sim ranges between -1 and 1, where 1 is most similar
-			bestSim = 0
-			bestWord = "nothing"
-			
-			# look at all word representations to find the answer to a:b c:bestWord
-			# except for a, b and c
-			for word in wordvectors:
-				if word not in question:
-
-					# again assume that there is only one projection for the word
-					wordRep = wordvectors[word][0]
-
-					# similarity is defined as the cosine similarity
-					# cosine similarity normaly is (a (dot product) b) / (norm(a) * norm(b))
-					# we have normalized a and b, so the denominator is always one and can be discarded
-					sim = sum([y[i] * wordRep[i] for i in xrange(len(y))])
-
-					# save result if it is better than the previous best result
-					if sim > bestSim:
-						bestSim = sim
-						bestWord = word
-		
-		# If we don't have a projection for a, b, or c, we won't be answering the question.
-		else:
-			bestWord = 'nothing'
-		answers.append(bestWord)
-		print question[0], ' ', question[1], ' ', question[2], ' ', bestWord
-	return answers
+				y = normalize([b[i] - a[i] + c[i] for i in xrange(len(a))])
+				sim += sum([y[i] * d[i] for i in xrange(len(y))])
+			else:
+				sim = -10
+		sim = sim / float(len(questions))
+		print "Appending ", answer
+		ranking.append((sim, answer))
+	return sorted(ranking, key = lambda x: x[0], reverse = True)
 
 
 if __name__ == "__main__":
-	if not len(sys.argv) == 3:
+	if not len(sys.argv) == 4:
 		print "Call me as:"
-		print "python amb_eval.py wordvectors.txt questions"
+		print "python semEvalQA.py <wordvectors> <questions> <answers>"
 		sys.exit()
 
-	questions = sys.argv[2]
+	projections = sys.argv[1]
+	questionsFile = sys.argv[2]
+	answersFile = sys.argv[3]
 
 	print "Loading questions..."
-	questions = load_questions(questions)
+	questions = load_questions(questionsFile)
+	print "Loaded ", len(questions), " questions."
+
+	print "Loading answers..."
+	answers = load_answers(answersFile)
+	print "Loaded ", len(answers), " answers"
 	
 	print "Loading word projections"
-	vecs = load_vectors(sys.argv[1])
+	vecs = load_vectors(projections)
 	
 	print "Answering questions"
-	answers = qa(vecs, questions)
+	ranking = qa(vecs, questions, answers)
 	
 	print "Saving answers to file"
-	save_answers(answers, "precomputedAnswers/" + sys.argv[1].split("/")[-1] + "." + sys.argv[2].split("/")[-1].split(".")[0] + ".answered")
-	#save_answers(answers, "test")
+	save_answers(ranking, "SemEvalRankings/" + projections.split("/")[-1] + "." + questionsFile.split("/")[-1].split(".")[0] + ".answered")
+	#save_answers(ranking, "test")
